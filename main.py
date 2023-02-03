f2f306a6dfa24ccca9da50ffbee2bd77110c8675
akaz <82073147+akazdayo@users.noreply.github.com>
2023-02-03 20:35:15 +0900

    githubのURLを追加しました

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ import random
 import numpy as np
 import pandas as pd
 
+daikiti = 0
+syoukiti = 0
+kyou = 0
+
 with st.spinner('Wait for it...'):
     time.sleep(1)
 kuji = random.randint(0, 2)
 
-daikiti = 0
-syoukiti = 0
-kyou = 0
+st.write("Source : https://github.com/akazdayo/spinner_omiikuji")
 
 if kuji == 0:
     st.success("大吉")
