d4fb0264c0082fc0f866d7ac211f456fcdd32271
akaz <82073147+akazdayo@users.noreply.github.com>
2023-02-03 20:27:15 +0900

    結果を「大吉」,「笑吉」,「凶」に変更しました

---
+++ README.md
@@ -0,0 +1,7 @@
+# おみくじサイト
+https://akazdayo-spinner-omiikuji-main-igw0fs.streamlit.app
+
+# 実行方法
+1. ``pipenv install``
+1. ``pipenv shell``
+1. ``streamlit run main.py``
--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 import time
 import streamlit as st
 import random
+import numpy as np
+import pandas as pd
 
 with st.spinner('Wait for it...'):
-    time.sleep(5)
-rand = random.randint(0, 2)
-if rand == 0:
-    st.success("Done")
-elif rand == 1:
-    st.warning("Warning")
+    time.sleep(1)
+kuji = random.randint(0, 2)
+
+daikiti = 0
+syoukiti = 0
+kyou = 0
+
+if kuji == 0:
+    st.success("大吉")
+    daikiti += 1
+elif kuji == 1:
+    st.warning("笑吉")
+    syoukiti += 1
 else:
-    st.error("Error")
+    st.error("凶")
+    kyou += 1
+
 st.balloons()
