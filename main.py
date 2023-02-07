719ad36bd488343a7c529511cf455db1e8f95776
akaz <82073147+akazdayo@users.noreply.github.com>
2023-02-07 17:44:33 +0900

    グラフの追加

--- main.py
+++ main.py
@@ -8,20 +8,46 @@ daikiti = 0
 syoukiti = 0
 kyou = 0
 
+chart = []
+
+st.title("🎊おみくじグラフ🎊")
+
 with st.spinner('Wait for it...'):
     time.sleep(1)
-kuji = random.randint(0, 2)
+
 
 st.write("Source : https://github.com/akazdayo/spinner_omiikuji")
 
-if kuji == 0:
-    st.success("大吉")
-    daikiti += 1
-elif kuji == 1:
-    st.warning("笑吉")
-    syoukiti += 1
-else:
-    st.error("凶")
-    kyou += 1
+chart = [0, 0, 0]
+daikiti = 0
+syoukiti = 0
+kyou = 0
+
+values = st.slider(
+    '回数',
+    0, 10000, 100)
+
+c = st.container()
+
+
+with st.expander("おみくじ結果一覧"):
+    for i in range(values):
+        kuji = random.randint(0, 2)
+        chart[kuji] += 1
+        if kuji == 0:
+            st.success("大吉")
+            daikiti += 1
+        elif kuji == 1:
+            st.warning("笑吉")
+            syoukiti += 1
+        else:
+            st.error("凶")
+            kyou += 1
+
+chart_data = pd.DataFrame(
+    chart,
+    index=['大吉', '笑吉', '凶'],
+)
+c.bar_chart(chart_data)
 
 st.balloons()
